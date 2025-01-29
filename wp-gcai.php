b253caaa2ef32b1294881ab21d41122738b1f697
hadiniaraki <hadi.jafari.niaraki@gmail.com>
2025-01-29 13:03:25 +0330

    add header requirements to main file

---
+++ wp-gcai.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: wp-gcai
+ * Plugin URI: https://wp.gcai.ir
+ * Description: gcai is a WordPress plugin that leverages artificial intelligence to help users generate text content and video scripts. By integrating OpenAI APIs, this plugin simplifies and streamlines the content creation process. With gcai, users can quickly generate articles and other content by filling out simple forms on the website.
+ * Version: 1.0.0
+ * Author: Hadi Jafari Niaraki
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: gcai
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
